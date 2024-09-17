09c054d16151801dfe8fa00a342b59816e467945
OzanUlus <ilkerozanulus@gmail.com>
2024-09-17 14:32:45 +0300

    added response

---
+++ Core/CarBooking.Application/Response/Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Application.Response
+{
+    public class Response<T> : IResponse<T>
+    {
+        public bool Success { get ; set ; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public Response(bool success, string message, T data )
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static Response<T> SuccessResponse(T data, string message) 
+        {
+            return new Response<T>(true,message,data);
+        }
+
+        public static Response<T> ErrorResponse(string message) 
+        {
+            return new Response<T>(false,message,default);
+        }
+
+       
+    }
+}
