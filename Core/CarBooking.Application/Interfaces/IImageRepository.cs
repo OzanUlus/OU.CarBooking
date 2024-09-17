4d9c137b2ced482542530a7283fbae3dc69d2d03
OzanUlus <ilkerozanulus@gmail.com>
2024-09-17 12:03:41 +0300

    IRepository generic and other interfaces

---
+++ Core/CarBooking.Application/Interfaces/IImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using CarBooking.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Application.Interfaces
+{
+    public interface IImageRepository : IRepository<Image>
+    {
+    }
+}
